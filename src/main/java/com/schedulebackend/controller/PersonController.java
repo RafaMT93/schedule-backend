0606eefb85d29c05f9bf6af5bef7a370fc8f52fc
RafaMT93 <rafat93.rdm@outlook.com.br>
2024-04-10 17:50:52 -0300

    feat: new controller

--- src/main/java/com/schedulebackend/controller/PersonController.java
+++ src/main/java/com/schedulebackend/controller/PersonController.java
@@ -1,12 +1,57 @@
 package com.schedulebackend.controller;
 
+import com.schedulebackend.DTO.PersonDTO;
+import com.schedulebackend.model.Person;
+import com.schedulebackend.service.PersonService;
+import com.schedulebackend.util.DateUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("person")
 @Log4j2
+@RequiredArgsConstructor
 public class PersonController {
+    private final DateUtil dateUtil;
+    private final PersonService personService;
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPersons() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(personService.getAllPersons());
+    }
+
+    @GetMapping(path = "/{guid}")
+    public ResponseEntity<Person> getPerson(@PathVariable String guid) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(personService.getPersonByGuid(guid));
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody PersonDTO personDTO) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        Person person = personService.createPerson(personDTO);
+        return ResponseEntity.ok(person);
+    }
+
+    @DeleteMapping(path = "/{guid}")
+    public ResponseEntity<Person> deletePerson(@PathVariable String guid) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        personService.deletePerson(guid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updatePerson(@RequestBody PersonDTO personDTO) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        personService.replacePerson(personDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
